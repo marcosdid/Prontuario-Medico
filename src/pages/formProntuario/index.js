caee5f654b292874c6c97962f2526c30038bc112
marcosdid <limafilhoguerreiro@outlook.com>
2020-09-01 23:10:47 -0300

    creating folder structure of pages

---
+++ src/pages/formProntuario/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.css'
+
+const Form = () => {
+  return (
+    <div>
+      <h1>Ola mundo</h1>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const Route = () => {
+  ;<BrowserRouter>
+    <Switch>
+      <Route path='/list' />
+      <Route path='/cadastro' />
+    </Switch>
+  </BrowserRouter>
+}
