745c733b44fa4f67ee80aef870fee0c0a3b0cd88
marcosdid <limafilhoguerreiro@outlook.com>
2020-09-02 16:17:12 -0300

    add feature Select Items

--- src/pages/form-prontuario/index.js
+++ src/pages/form-prontuario/index.js
@@ -1,21 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Select from '../../components/Select'
 import Button from '../../components/Button'
 import Textarea from '../../components/Textarea'
 
+import api from '../../services/api'
+
+import deleteIcon from '../../assets/icons/x.svg'
+
 import './styles.css'
 
 const Form = () => {
-  const [queixa, setQueixa] = useState({})
-  const [doenca, setDoenca] = useState({})
+  const [queixas, setQueixas] = useState([])
+  const [doencas, setDoencas] = useState([])
+
+  const [userQueixa, setUserQueixa] = useState({})
+  const [userDoenca, setUserDoenca] = useState({})
   const [historico, setHistorico] = useState('')
+
+  const [selects, setSelects] = useState([])
 
   function test(e) {
     e.preventDefault()
 
-    console.log({ queixa, doenca, historico })
+    console.log({ userQueixa, userDoenca, historico })
   }
+
+  function handleDeleteSelectedItem(id) {
+    const selecionados = selects.filter((item, index) => {
+      return index !== id
+    })
+    setSelects(selecionados)
+  }
+
+  useEffect(() => {
+    async function getApi() {
+      const queixas = await api.get('/queixas')
+      const doencas = await api.get('/doencas')
+      setQueixas(queixas.data.data)
+      setDoencas(doencas.data.data)
+    }
+
+    getApi()
+  }, [])
 
   return (
     <div className='container'>
@@ -28,34 +55,41 @@ const Form = () => {
           <Select
             name='Queixa'
             label='Queixa Principal'
-            inputValue={queixa}
+            value={userQueixa}
             onChange={(e) => {
-              setQueixa(e.target.value)
+              setUserQueixa(e.target.value)
             }}
-            options={[
-              { value: 1, label: 'Dor de cabeça' },
-              { value: 2, label: 'Dor nas costas' },
-            ]}
+            options={queixas}
           />
 
           <Select
             name='Doenças'
             label='Doenças Adulto'
-            inputValue={doenca}
+            value={userDoenca}
             onChange={(e) => {
-              setDoenca(e.target.value)
+              setUserDoenca(e.target.value)
+              setSelects([...selects, { id: e.target.value - 1 }])
             }}
-            options={[
-              { value: 1, label: 'Diabetes' },
-              { value: 2, label: 'Câncer' },
-            ]}
+            options={doencas}
           />
 
           <div className='selecionados'>
             <label>Selecionados:</label>
             <div className='options'>
-              <div>Câncer X</div>
-              <div>Diabetes X</div>
+              {selects.map((select, index) => {
+                return (
+                  <div key={select.id} className='select'>
+                    <p>{doencas[select.id].label}</p>
+                    <img
+                      src={deleteIcon}
+                      alt='delete'
+                      onClick={() => {
+                        handleDeleteSelectedItem(index)
+                      }}
+                    />
+                  </div>
+                )
+              })}
             </div>
           </div>
 
