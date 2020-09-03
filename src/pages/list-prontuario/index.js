e351f0c089141de5ef5a1929af67b2b2a59adba4
marcosdid <limafilhoguerreiro@outlook.com>
2020-09-02 22:21:20 -0300

    add component Pront in list-prontuario

--- src/pages/list-prontuario/index.js
+++ src/pages/list-prontuario/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import Button from '../../components/Button'
+import Pront from '../../components/prontuario'
 
 import './styles.css'
 
 const List = () => {
   return (
     <div id='container'>
-      <p>Nenhum prontuário cadastrado.</p>
-
+      <p className='para'>Nenhum prontuário cadastrado.</p>
+      <Pront />
       <Button type='button'>Adicionar novo prontuário</Button>
     </div>
   )
