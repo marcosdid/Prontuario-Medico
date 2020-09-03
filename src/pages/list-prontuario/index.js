54d1e728f5877d7200327c6bbd4ddafe43575960
marcosdid <limafilhoguerreiro@outlook.com>
2020-09-03 01:17:05 -0300

    add new Prontuario

--- src/pages/list-prontuario/index.js
+++ src/pages/list-prontuario/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import Button from '../../components/Button'
 import Pront from '../../components/prontuario'
@@ -6,11 +7,47 @@ import Pront from '../../components/prontuario'
 import './styles.css'
 
 const List = () => {
+  const [prontuarios, setProntua] = useState([])
+
+  const history = useHistory()
+
+  const indices = Object.keys(localStorage)
+
+  useEffect(() => {
+    const pronts = indices.map((key) => {
+      return JSON.parse(localStorage.getItem(key))
+    })
+
+    setProntua(pronts)
+  }, [])
+
   return (
     <div id='container'>
-      <p className='para'>Nenhum prontuário cadastrado.</p>
-      <Pront />
-      <Button type='button'>Adicionar novo prontuário</Button>
+      <div className='block-prontuarios'>
+        {indices[0] ? (
+          prontuarios.map((item) => {
+            return (
+              <Pront
+                date={item.created_at}
+                queixa={item.queixa}
+                doencas={item.doencas}
+                historico={item.historico}
+              />
+            )
+          })
+        ) : (
+          <p className='para'>Nenhum prontuário cadastrado.</p>
+        )}
+
+        <Button
+          type='button'
+          onClick={() => {
+            history.push('/cadastro')
+          }}
+        >
+          Adicionar novo prontuário
+        </Button>
+      </div>
     </div>
   )
 }
